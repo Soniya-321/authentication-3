227bf16359149e1335854e3f23563ea0411966c0
Borlakunta Soniya <155159154+Soniya-321@users.noreply.github.com>
2024-07-19 10:09:34 +0530

    main

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const initializeDBAndServer = async () => {
       driver: sqlite3.Database,
     })
     app.listen(3000, () => {
-      console.log('Server Running at http://localhost:4000/')
+      console.log('Server Running at http://localhost:3000/')
     })
   } catch (e) {
     console.log(`DB Error: ${e.message}`)
